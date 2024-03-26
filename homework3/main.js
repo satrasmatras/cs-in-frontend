539a5bb8f8229bec16a22bcbf593c2e6b799bc8d
denis.gorshkov <satrasmatrassatras@gmail.com>
2024-03-26 16:57:06 +0300

    feature(editorconfig): add editorconfig and reformat existing code

--- homework3/main.js
+++ homework3/main.js
@@ -1,23 +1,23 @@
-function *fizzbuzz() {
-    let i = 1n;
+function* fizzbuzz() {
+  let i = 1n;
 
-    while (true) {
-        if (i % 15n === 0n) {
-            yield 'FizzBuzz';
-        } else if (i % 3n === 0n) {
-            yield 'Fizz';
-        } else if (i % 5n === 0n) {
-            yield 'Buzz';
-        } else {
-            yield i;
-        }
-
-        i += 1n;
+  while (true) {
+    if (i % 15n === 0n) {
+      yield 'FizzBuzz';
+    } else if (i % 3n === 0n) {
+      yield 'Fizz';
+    } else if (i % 5n === 0n) {
+      yield 'Buzz';
+    } else {
+      yield i;
     }
+
+    i += 1n;
+  }
 }
 
 const myFizzBuzz = fizzbuzz();
 
-for (let i = 0; i < 100; i+=1) {
-    console.log(myFizzBuzz.next());
+for (let i = 0; i < 100; i += 1) {
+  console.log(myFizzBuzz.next());
 }
